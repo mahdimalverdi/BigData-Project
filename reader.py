d9e416850a9c1a31125b2edb7b819c95d1b19b90
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-08-15 18:53:52 +0430

    add selenuim

--- reader.py
+++ reader.py
@@ -6,7 +6,6 @@ consumer = KafkaConsumer(
      bootstrap_servers=['localhost:9092'],
      auto_offset_reset='earliest',
      enable_auto_commit=True,
-     group_id='my-group',
      value_deserializer=lambda x: loads(x.decode('utf-8')))
 
 for message in consumer:
