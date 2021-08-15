b9ab17acd60b17d56f003a126b4561abf8807172
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-08-15 18:08:55 +0430

    add selenuim

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from json import dumps
 from kafka import KafkaConsumer, KafkaProducer
 from json import loads
 
-producer = KafkaProducer(bootstrap_servers='185.235.40.116:9092')
+producer = KafkaProducer(bootstrap_servers=['185.235.40.116:9092'])
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
@@ -23,14 +23,13 @@ while driver.execute_script("return (true)"):
     print('3')
     if(last_tweet != tweet):
         x = dumps(tweet).encode('utf-8')
-        result =  producer.send(topic='quickstart-events', value=b'x')
+        producer.send('sample', b'Hello, World!').get(10)
+        producer.send('sample', key=b'message-two', value=b'This is Kafka-Python').get(10)
         print("tweet: " + tweet )
         username = driver.find_element(By.XPATH, "//app-user-status-bar/a").text
         print("username: " + username)
         last_tweet = tweet
         print("---------------------------------")
-        r = producer.flush()
-        x = 0
     time.sleep(.01)
 
 #
