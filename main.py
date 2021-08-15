e87d5a809296c349c68981d1220bea7478f1388d
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-08-15 17:00:02 +0430

    replace

---
+++ main.py
@@ -0,0 +1,31 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# import time
+# from json import dumps
+# from kafka import KafkaConsumer, KafkaProducer
+# from json import loads
+#
+# producer = KafkaProducer(bootstrap_servers='185.235.40.116:9092')
+#
+# driver = webdriver.Chrome()
+# driver.get("https://www.sahamyab.com/stocktwits")
+# last_tweet = ''
+# while driver.execute_script("return (true)"):
+#     tweet = driver.find_element(By.XPATH, "//div[2]/p").text
+#     if(last_tweet != tweet):
+#         x = dumps(tweet).encode('utf-8')
+#         result =  producer.send(topic='quickstart-events', value=b'x')
+#         print("tweet: " + tweet )
+#         username = driver.find_element(By.XPATH, "//app-user-status-bar/a").text
+#         print("username: " + username)
+#         last_tweet = tweet
+#         print("---------------------------------")
+#         r = producer.flush()
+#         x = 0
+#     time.sleep(.01)
+#
+
+from kafka import KafkaProducer
+producer = KafkaProducer(bootstrap_servers=['185.235.40.116:9092'])
+producer.send('sample', b'Hello, World!').get(10)
+producer.send('sample', key=b'message-two', value=b'This is Kafka-Python').get(10)
---
+++ reader.py
@@ -0,0 +1,21 @@
+# from kafka import KafkaConsumer, KafkaProducer
+# from json import loads
+#
+# consumer = KafkaConsumer('quickstart-events',
+#                          bootstrap_servers=['185.235.40.116:9092'],
+#                          auto_offset_reset='earliest',
+#                          enable_auto_commit=True,
+#                          value_deserializer=lambda x: loads(x.decode('utf-8')))
+# for msg in consumer:
+#     print (msg)
+
+# from kafka import KafkaProducer
+# producer = KafkaProducer(bootstrap_servers='185.235.40.116:9092')
+# producer.send('quickstart-events', b'Hello, World!')
+# producer.send('quickstart-events', key=b'message-two', value=b'This is Kafka-Python')
+
+from kafka import KafkaConsumer
+consumer = KafkaConsumer('sample',bootstrap_servers=['185.235.40.116:9092'])
+consumer.next()
+for message in consumer:
+    print (message)
