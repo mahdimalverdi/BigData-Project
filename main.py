c9af119d9413bf36014058f3be5e34871fa6078f
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-08-15 17:46:42 +0430

    try fix

--- main.py
+++ main.py
@@ -7,7 +7,21 @@ from json import loads
 
 producer = KafkaProducer(bootstrap_servers='185.235.40.116:9092')
 
-driver = webdriver.Chrome()
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
+chromeOptions.add_argument("--no-sandbox")
+chromeOptions.add_argument("--disable-setuid-sandbox")
+
+chromeOptions.add_argument("--remote-debugging-port=9222")  # this
+
+chromeOptions.add_argument("--disable-dev-shm-using")
+chromeOptions.add_argument("--disable-extensions")
+chromeOptions.add_argument("--disable-gpu")
+chromeOptions.add_argument("start-maximized")
+chromeOptions.add_argument("disable-infobars")
+chromeOptions.add_argument(r"user-data-dir=.\cookies\\test")
+
+driver = webdriver.Chrome(chrome_options=chromeOptions)
 driver.get("https://www.sahamyab.com/stocktwits")
 last_tweet = ''
 while driver.execute_script("return (true)"):
