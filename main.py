efd24ee8e1fde52ad4b9c03041c72bca1d8af65d
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-08-15 17:48:56 +0430

    fix

--- main.py
+++ main.py
@@ -11,15 +11,7 @@ chromeOptions = webdriver.ChromeOptions()
 chromeOptions.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
 chromeOptions.add_argument("--no-sandbox")
 chromeOptions.add_argument("--disable-setuid-sandbox")
-
-chromeOptions.add_argument("--remote-debugging-port=9222")  # this
-
 chromeOptions.add_argument("--disable-dev-shm-using")
-chromeOptions.add_argument("--disable-extensions")
-chromeOptions.add_argument("--disable-gpu")
-chromeOptions.add_argument("start-maximized")
-chromeOptions.add_argument("disable-infobars")
-chromeOptions.add_argument(r"user-data-dir=.\cookies\\test")
 
 driver = webdriver.Chrome(chrome_options=chromeOptions)
 driver.get("https://www.sahamyab.com/stocktwits")
