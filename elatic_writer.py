f16b44bca55d126a004f4a2458161dc6b643fb5c
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-08-17 12:36:30 +0430

    fix

--- elatic_writer.py
+++ elatic_writer.py
@@ -8,16 +8,12 @@ consumer = KafkaConsumer(
      bootstrap_servers=['185.235.40.116:9092'],
      enable_auto_commit=True,
      value_deserializer=lambda x: loads(x.decode('utf-8')),
+    auto_offset_reset ='earliest',
     api_version=(0, 10, 1))
 
 for message in consumer:
     value = message.value
     es.index(index='tweets', doc_type='tweet', body=value)
-    for item in value['tokens']:
-        token = value
-        token['token'] = item
-        es.index(index='tokens', doc_type='tweet', body=token)
-        print(item)
     print (value)
 
 
