d99f9cf0c998249c3849e062197079aba31c59f1
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-08-17 12:21:20 +0430

    fix

--- main.py
+++ main.py
@@ -5,29 +5,48 @@ import time
 from json import dumps
 from kafka import KafkaConsumer, KafkaProducer
 from json import loads
+from selenium.webdriver.support import expected_conditions as EC
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda x: dumps(x).encode('utf-8'))
+from selenium.webdriver.support.wait import WebDriverWait
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome("/usr/bin/chromedriver",chrome_options=chrome_options)
-driver.get("https://www.sahamyab.com/stocktwits")
-last_tweet = ''
-while driver.execute_script("return (true)"):
-    tweet = driver.find_element(By.XPATH, "//div[2]/p").text
-    if(last_tweet != tweet):
-        print("tweet: " + tweet )
-        username = driver.find_element(By.XPATH, "//app-user-status-bar/a").text
-        print("username: " + username)
-        producer.send('sample', {'tweet':tweet, 'user': username}).get(10)
-        last_tweet = tweet
-        print("---------------------------------")
-    time.sleep(.01)
-
+producer = KafkaProducer(bootstrap_servers=['185.235.40.116:9092'], value_serializer=lambda x: dumps(x).encode('utf-8'))
 #
-# from kafka import KafkaProducer
-# producer = KafkaProducer(bootstrap_servers=['185.235.40.116:9092'])
-# producer.send('sample', b'Hello, World!').get(10)
-# producer.send('sample', key=b'message-two', value=b'This is Kafka-Python').get(10)
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--disable-dev-shm-usage')
+# driver = webdriver.Chrome("/usr/bin/chromedriver",chrome_options=chrome_options)
+driver = webdriver.Chrome()
+driver.get("https://twitter.com/login")
+
+with open('Include/config', encoding='utf-8') as f:
+    configs = [word.strip() for word in f.readlines()]
+
+driver.find_element(By.NAME, "session[username_or_email]").send_keys(configs[0])
+driver.find_element(By.NAME, "session[password]").send_keys(configs[1])
+driver.find_element(By.XPATH, "//div[3]/div/div").click()
+wait = WebDriverWait(driver, 1000)
+element = wait.until(EC.element_to_be_clickable((By.XPATH, '//article/div/div/div/div[2]/div[2]/div[2]')))
+
+while True:
+
+    for i in range(1, 100000):
+        try:
+            tweet = driver.find_element(By.XPATH, "(//article/div/div/div/div[2]/div[2]/div[2]/div[1])["+str(i)+"]").text
+            display_name = driver.find_element(By.XPATH, "(//article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]/div[1]/a/div/div[1])["+str(i)+"]").text
+            user_name = driver.find_element(By.XPATH, "(//article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]/div[1]/a/div/div[2])["+str(i)+"]").text
+            datetime = driver.find_element(By.XPATH, "(//article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]/a/time)["+str(i)+"]").get_attribute("datetime")
+            print("username: " + user_name)
+            print("display name: " + display_name)
+            print("time: " + datetime)
+            producer.send('sample', {
+                'tweet': tweet,
+                'user': user_name,
+                'display_name': display_name,
+                'time': datetime
+            })
+            print("---------------------------------")
+        except:
+            break;
+    driver.execute_script("window.scrollBy(0,100000)")
+    time.sleep(1)
--- reader.py
+++ reader.py
@@ -1,12 +1,40 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 from json import loads
+from parsivar import Normalizer
+from parsivar import Tokenizer
+from parsivar import FindStems
+from json import dumps
 
 consumer = KafkaConsumer(
     'sample',
-     bootstrap_servers=['localhost:9092'],
-     auto_offset_reset='earliest',
+     bootstrap_servers=['185.235.40.116:9092'],
      enable_auto_commit=True,
-     value_deserializer=lambda x: loads(x.decode('utf-8')))
+     value_deserializer=lambda x: loads(x.decode('utf-8')),
+    api_version=(0, 10, 1))
+
+producer = KafkaProducer(bootstrap_servers=['185.235.40.116:9092'], value_serializer=lambda x: dumps(x).encode('utf-8'))
+
+my_normalizer = Normalizer(statistical_space_correction=True)
+my_tokenizer = Tokenizer()
+my_stemmer = FindStems()
+
+stopwords = set()
+with open('Include/stopwords.txt', encoding='utf-8') as f:
+    stopwords = set([word.strip() for word in f.readlines()])
+
+
 
 for message in consumer:
-    print (message)
+    value = message.value
+    cleaned_sentence = my_normalizer.normalize(value['tweet'])
+    tokens = my_tokenizer.tokenize_words(cleaned_sentence)
+    stems = []
+
+    for word in [my_stemmer.convert_to_stem(item) for item in tokens]:
+        if word not in stopwords:
+            stems.append(word)
+
+    value['cleaned_tweet'] = cleaned_sentence
+    value['tokens'] = list(set(stems))
+    producer.send('cleaneddata', value)
+    print (value)
